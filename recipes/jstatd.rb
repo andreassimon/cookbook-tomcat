053fc612286f35172b1cb852860ed9c4177cb752
Andreas Simon <a.simon@quagilis.de>
2014-03-13 16:56:43 +0100

    Added jstatd init.d script

--- recipes/jstatd.rb
+++ recipes/jstatd.rb
@@ -5,3 +5,20 @@ template "#{node['java']['java_home']}/jre/lib/security/jstatd.policy" do
   group "143"
   mode 00644
 end
+
+init_script_path = "/etc/init.d/jstatd"
+template init_script_path do
+  source "init.d_jstatd.erb"
+  owner "root"
+  group "root"
+  mode 00755
+  variables(
+    java_home: node['java']['java_home'],
+    init_script_path: init_script_path
+  )
+  notifies [:enable, :start], "service[jstatd]"
+end
+
+service "jstatd" do
+  action :nothing
+end
